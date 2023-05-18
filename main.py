ebad8c9f6748ee65d10f209804bc0556dc312a57
Aleksandr <rakov.alieksandr@inbox.ru>
2023-05-18 21:01:33 +0300

    начало

---
+++ main.py
@@ -0,0 +1,112 @@
+# n = 5
+
+# print(n)
+
+# n = "dfddf\"gfg"
+
+# print(n)
+
+# print(5,6,5)
+
+# # print(5,6,9)
+
+# print(5,6)
+
+# """print(5,6,5)
+# print(5,6,5)"""
+
+# print(5,6,5)
+
+
+# a = 5
+# b = 5.89
+# c = "hello"
+
+# print(a,b,c)
+
+# print(f"{a} - {b} - {c}")
+
+# print("{} - {} - {}".format(a,b,c))
+
+# print("Введите первое число: ")
+# a = input()
+# print(a)
+
+# print("Введите первое число: ")
+# a = input()
+
+# b = input(" Введите второе число: ")
+
+# print("Введите первое число: ")
+# a = input()
+
+# b = input(" Введите второе число: ")
+# print (a, " + ", b, " = ",a + b)
+
+# c =  5.89
+# print(c)
+# n = int(c)
+# print(n)
+
+# c =  5.89
+# print(c)
+# print(type(c))
+# c = int(c)
+# print(c)
+# print(type(c))
+
+# print("Введите первое число: ")
+# a = int(input())
+
+# b = int(input(" Введите второе число: "))
+# print (a, " + ", b, " = ",a + b)
+
+# a = 5.8989
+# b = 6.89898
+# print(round(a*b,3))
+
+
+# a = 1 < 3 < 5 < 10
+# print(a)
+
+
+# username = input("Введите имя: ")
+# if username == "Маша":
+#     print("Ура, это же Маша!")
+# elif username == "Марина":
+#     print ("Я так ждал вас, Марина!")
+# elif username == "Ильнар":
+#     print("Ильнар - топ!")
+# else:
+#     print("Привет,",username)    
+
+
+# i = 0
+# while i < 5:
+#     # if i == 3:
+#     #     break
+#     i = i + 1
+# else:
+#     print("( Пожалуй")
+#     print("хватит )")    
+# print(i)
+
+
+# a = "qwerty"
+# for i in a:
+#     print(i)
+
+
+# line = ""
+# for i in range(5):
+#     line = ""
+#     for j in range(5):
+#         line += "*"
+#     print(line)     
+
+text = "Сьешь  еще что - НибудЬ вкуСноЕ наВерНое"
+print(len(text))
+print("еще" in text)
+print(text.lower())
+print(text.upper())
+print(text.replace("еще", "ЕЩЕ"))
