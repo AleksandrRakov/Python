1b21a9d9e4a0895db0ed7beffbf5ac8dde3bcb39
AleksandrRakov <rakov.alieksandr@inbox.ru>
2023-06-01 16:26:33 +0300

    Домашнее задание 1.06

--- task22/task22.py
+++ task22/task22.py
@@ -9,3 +9,17 @@
 # 2 4 6 8 10 12 10 8 6 4 2
 # 3 6 9 12 15 18
 # 6 12
+
+
+from random import randint
+n = int(input("Введите кол-во чисел первого множества: "))
+list_1 = [randint(0,50) for i in range(n)]
+m = int(input("Введите кол-во чисел второго множества: "))
+list_2 = [randint(0,50) for i in range(m)]
+print(*list_1)
+print(*list_2)
+result = [int(num) for num in list_1 if num in list_2]
+if result == []:
+    print("Повторяющихся чисел нет. ")
+else:
+    print("Повторяющиеся числа: ", *result)
