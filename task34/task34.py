084152901c6e277d66e43c40ef0a68a10264ab31
Aleksandr <rakov.alieksandr@inbox.ru>
2023-06-12 13:34:54 +0300

    домашка 9.06

--- task34/task34.py
+++ task34/task34.py
@@ -27,3 +27,19 @@ else:
     print('Пам парам')  
 
 
+
+# text = input().lower().split()
+# result = set()
+# vowels = 'ёеаоэяиюуы'
+# for word in text:
+#     count = 0
+#     for char in word:
+#         if char in vowels:
+#             count += 1
+#     result.add(count)
+# if len(result) == 1:
+#     print("Парам пам-пам")
+# else:
+#     print("Пам парам")
+
+
